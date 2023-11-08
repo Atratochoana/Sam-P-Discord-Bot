e7386a59faea6e2dcccebbf634b7a58c3d091bb6
Ralph <Ralphy.t.small@gmail.com>
2023-11-08 15:27:59 +0000

    Giveaway Button

--- src/buttons/misc/Giveaway/join-giveaway.js
+++ src/buttons/misc/Giveaway/join-giveaway.js
@@ -1,3 +1,20 @@
+const { Client, Interaction, ApplicationCommandOptionType, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js");
+
+/**
+ * 
+ * @param {Client} client 
+ * @param {Interaction} interaction 
+ */
+
+
 module.exports = (client,interaction) => {
-    console.log("worked")
+
+
+    interaction.reply({
+        ephemeral: true,
+        content: `You have been registered for the giveaway. :tada:`
+    });
+
+    
+
 }
--- src/commands/economy/level.js
+++ src/commands/economy/level.js
@@ -13,6 +13,7 @@ module.exports = {
             type: ApplicationCommandOptionType.Mentionable,
         }
     ],
+    deleted: true,
 
     /**
      * 
