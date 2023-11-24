c5bb7c0c36ba8c390127d1346b565534dad74707
Atratochoana <Ralphy.t.small@gmail.com>
2023-11-24 23:50:31 +0000

    IDFK

---
+++ src/commands/logging/createWebhook.js
@@ -0,0 +1,35 @@
+const { Client, Interaction, ApplicationCommandOptionType} = require("discord.js");
+
+module.exports = {
+    name: 'logging-channel',
+    description: "Creates a channel to log commands in this server",
+    userPermissions: true,
+    permissionLevel: 3,
+    options: [{
+            name: 'channel',
+            description: 'what channel you want the webhook to log to',
+            type: ApplicationCommandOptionType.Channel,
+            required: true,
+        },
+    ],
+
+    /**
+     * 
+     * @param {Client} client 
+     * @param {Interaction} interaction 
+     */
+
+    callback: (client,interaction) => {
+        channel = interaction.options.getChannel("channel")
+        channel.createWebhook({
+            name: "Sam-P Logging",
+        }).then(webhook => webhook.send(`Created webhook by <@${interaction.user.id}>`))
+        // add a funcntionality to add to DB with guild id and webhook id so it can be called.
+
+        interaction.reply({
+            content: 'Webhook has been made, Its not recommended to make multiple webhook\' in the same channel',
+            ephemeral: true
+        })
+
+    }
+}
---
+++ src/utils/logInteraction.js
@@ -0,0 +1,7 @@
+WEBHOOK = "" // need to add a function to check db for if there is a custom webhook for the server or not / for now this works for testing. // This is the universal log
+
+module.export = {
+
+
+    
+}
